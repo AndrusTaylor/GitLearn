360c49b8462e39ace883471a3d9ea1d4a75a653b
AndrusTaylor <andrus.y.yan@gmail.com>
2020-05-19 07:59:42 -0700

    first commit

---
+++ onlytest/onlytest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  onlytest
+//
+//  Created by AndrusTaylor on 2020/5/19.
+//  Copyright © 2020 AndrusTaylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ onlytest/onlytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  onlytest
+//
+//  Created by AndrusTaylor on 2020/5/19.
+//  Copyright © 2020 AndrusTaylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
